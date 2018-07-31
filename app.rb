02ec5b59b3ead30430d38afee7ff70996d563f64
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-31 18:44:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
     "Goodbye, #{@persons_name}."
   end
     
-  get '/addnumbers/:number1/:number2' do 
+  get '/addnumbers/:num1/:num2' do 
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
     (number1 * number2).to_i
