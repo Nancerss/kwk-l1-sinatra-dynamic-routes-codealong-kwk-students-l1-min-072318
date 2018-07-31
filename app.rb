ce3e7b3cc52d211b40584804bb06aa3af55a085e
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-31 18:43:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,8 +21,8 @@ class App < Sinatra::Base
   end
     
   get '/addnumbers/:number1/:number1' do 
-    number1 = params[:number1]
-    number2 = params[:number2]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
     
     
 end
